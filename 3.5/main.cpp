db98e228b8c5f3d60ae1ba957908a4690b6d2ee1
deefinan <deefinan@yahoo.ie>
2015-06-21 06:51:59 +0100

    Simple solution

---
+++ 3.5/main.cpp
@@ -0,0 +1,20 @@
+/**Print out prime numbers between 1 and 5000
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"1, ";
+   for(int i=2; i <= 5000; i++)
+   {
+       bool prime = false;
+       for(int j = 2; j < i; j++){
+            if(i%j == 0) break;
+            prime = true;
+       }
+       if(prime) cout<<i<< ", ";
+   }
+    return 0;
+}
