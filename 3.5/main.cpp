897f4f447630262cd9801733abd4cf87943b24a4
deefinan <deefinan@yahoo.ie>
2015-06-21 07:26:47 +0100

    correction of error in simple solution

--- 3.5/main.cpp
+++ 3.5/main.cpp
@@ -11,7 +11,10 @@ int main()
    {
        bool prime = false;
        for(int j = 2; j < i; j++){
-            if(i%j == 0) break;
+            if(i%j == 0) {
+                prime = false;
+                break;
+            }
             prime = true;
        }
        if(prime) cout<<i<< ", ";
