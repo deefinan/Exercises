6e017fa2eb3459f6c77845245f0c828d59c9bfc0
deefinan <deefinan@yahoo.ie>
2015-06-21 07:34:30 +0100

    more efficient solution

--- 3.5/main.cpp
+++ 3.5/main.cpp
@@ -7,10 +7,10 @@ using namespace std;
 int main()
 {
     cout<<"1, ";
-   for(int i=2; i <= 5000; i++)
+   for(int i=2; i <= 10; i++)
    {
        bool prime = false;
-       for(int j = 2; j < i; j++){
+       for(int j = 2; j <= (i/2)+1; j++){ //only neccesary to go as far as i2 as fractions cocme in pairs bigger and greater than i/2
             if(i%j == 0) {
                 prime = false;
                 break;
